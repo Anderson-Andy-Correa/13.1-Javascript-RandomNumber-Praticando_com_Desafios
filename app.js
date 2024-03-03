ddc536691d0b7edcbd0a5f37be83bc7c698fedc0
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-03 00:20:35 -0300

    Implementando o sorteador sem repetição e a função reiniciar

--- app.js
+++ app.js
@@ -11,13 +11,20 @@ function sortear() {
         };
         mostrarNumeros();
     } else {
-        alert('O número final deve ser maior que o número inicial');
+        if (ateNumero < deNumero) {
+            alert('O número final deve ser maior que o número inicial');
+        } else {
+            alert('Preenchas os campos corretamente!')
+        }
     };
 };
 
 function gerarNumeroAleatorio(deNumero, ateNumero) {
     let numeroSorteado = Math.floor(Math.random() * (ateNumero - deNumero + 1) + deNumero);
-    console.log(numeroSorteado);
+
+    if (listaNumeros.includes(numeroSorteado) && listaNumeros.length < (ateNumero - deNumero + 1)) {
+        return gerarNumeroAleatorio(deNumero, ateNumero);
+    }
     return numeroSorteado;
 };
 
@@ -28,3 +35,15 @@ function mostrarNumeros() {
     document.getElementById('btn-sortear').classList.replace('container__botao', 'container__botao-desabilitado');
     document.getElementById('btn-reiniciar').classList.replace('container__botao-desabilitado', 'container__botao');
 };
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+
+    let texto = `Números sorteados:  nenhum até agora`;
+    document.getElementById('texto_resultado').innerText = texto;
+
+    document.getElementById('btn-sortear').classList.replace('container__botao-desabilitado', 'container__botao');
+    document.getElementById('btn-reiniciar').classList.replace('container__botao', 'container__botao-desabilitado');
+}
